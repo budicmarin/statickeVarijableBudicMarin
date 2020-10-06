3f8d10c6dc787c9648eca43c57c931ff321c5b35
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-10-06 10:58:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statickeVarijableBudicMarin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa("Mirko", "Mirković");
+     
+            PrvaKlasa y= new PrvaKlasa("Ivica", "Ivić");
+            PrvaKlasa z = new PrvaKlasa();
+            Console.WriteLine(x+"\n"+y+"\n"+z);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrvaKlasa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statickeVarijableBudicMarin
+{
+    class PrvaKlasa
+    {
+        private string ime;
+        private string prezime;
+        private static int counter=0;
+        public PrvaKlasa()
+        {
+            counter++;
+            
+        }
+
+        public PrvaKlasa(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+        }
+
+        public int Counter
+        {
+            get { return counter; }
+            
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public override string ToString()
+        {
+            string ispis = "Redni broj: " + counter+", "+ime+" "+prezime;
+            return ispis;
+        }
+    }
+}
+
